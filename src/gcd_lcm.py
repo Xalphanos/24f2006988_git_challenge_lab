52d79f7ac6279bf6d0ba77640364db73f903fb6c
Xalphanos <hrisitroy4@gmail.com>
2025-09-17 21:56:50 +0530

    feat(number): gcd, lcm

---
+++ src/gcd_lcm.py
@@ -0,0 +1,13 @@
+﻿def gcd(a, b):
+    a, b = int(a), int(b)
+    while b:
+        a, b = b, a % b
+    return abs(a)
+
+def lcm(a, b):
+    a, b = int(a), int(b)
+    if a == 0 or b == 0:
+        return 0
+    from math import prod
+    g = gcd(a, b)
+    return abs(a*b) // g
