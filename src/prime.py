7081b37ffc7b939b6c11374d8c6cd3a53079d9e2
Xalphanos <hrisitroy4@gmail.com>
2025-09-17 21:56:43 +0530

    feat(prime): is_prime, next_prime

---
+++ src/prime.py
@@ -0,0 +1,18 @@
+﻿def is_prime(n):
+    n = int(n)
+    if n < 2:
+        return False
+    if n % 2 == 0:
+        return n == 2
+    i = 3
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += 2
+    return True
+
+def next_prime(n):
+    n = int(n) + 1
+    while not is_prime(n):
+        n += 1
+    return n
