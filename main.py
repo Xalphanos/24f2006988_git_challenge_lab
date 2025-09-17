c500f5a7c851c96b7a916fd19c8d18819dde740b
Xalphanos <hrisitroy4@gmail.com>
2025-09-17 21:27:45 +0530

    add main.py scaffold

---
+++ main.py
@@ -0,0 +1,9 @@
+﻿from src.sum import add
+from src.difference import sub
+from src.product import mul
+
+if __name__ == "__main__":
+    x, y = 7, 5
+    print("sum:", add(x, y))
+    print("difference:", sub(x, y))
+    print("product:", mul(x, y))
